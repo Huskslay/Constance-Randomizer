d865b24b91c24c98e52fae6f9f2bc507967f3caa
Huskslay <huskylover269@outlook.com>
2025-12-14 17:41:48 +0000

    Removed Cheats

--- RandomizerCore/Plugin.cs
+++ RandomizerCore/Plugin.cs
@@ -35,38 +35,4 @@ public class Plugin : BaseUnityPlugin
 
         Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
     }
-
-    private void Update()
-    {
-        // Fly
-        if (Keyboard.current.f9Key.wasPressedThisFrame)
-            ConDebugFlags.DebugFly = !ConDebugFlags.DebugFly;
-
-        // Invulnerability
-        if (Keyboard.current.f8Key.wasPressedThisFrame)
-            ConDebugFlags.DebugInvulnerability = !ConDebugFlags.DebugInvulnerability;
-
-        // Give all abilities and inspirations
-        if (Keyboard.current.f5Key.wasPressedThisFrame)
-        {
-            IConPlayerInventory inventoryManager = ConMonoBehaviour.SceneRegistry.Inventory;
-            IConPlayerEntity player = ConMonoBehaviour.SceneRegistry.PlayerOne;
-
-            string[] abilities = [
-                "slice", "wallDive", "dash", "doubleJump", "pogo", "stab", "bombClone", "fridaMask"
-            ];
-
-            foreach (string ability in abilities)
-                inventoryManager.Collect(player, CollectableHandler.dict[CollectableHandler.nameDict[ability]], 1);
-            foreach (SConCollectable_InspirationDrawing inspiration in CollectableHandler.inspirationCollectables)
-                inventoryManager.Collect(player, inspiration, 1);
-        }
-
-        if (Keyboard.current.f4Key.wasPressedThisFrame)
-        {
-            IConPlayerEntity player = ConMonoBehaviour.SceneRegistry.PlayerOne;
-            player.HealFully();
-            player.RefillPaint();
-        }
-    }
 }
